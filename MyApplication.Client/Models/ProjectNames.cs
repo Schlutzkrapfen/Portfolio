4b0e78f43f0d7cb27eb43c22daf2ddba93005a63
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-10-04 13:15:30 +0200

    added the porfolio

---
+++ MyApplication.Client/Models/ProjectNames.cs
@@ -0,0 +1,19 @@
+namespace Models
+{
+    public enum ProjectNames
+    {
+        Fading_lights = 0,
+        I_Have_Been_Bitten = 1,
+        Wired_West = 2,
+        Flying_Frogs = 3,
+        Inextinguishable = 4,
+        Määähdreascher = 5,
+        Cult_Of_Fire = 6,
+        Storm_Robber = 7,
+        Reflection_is_the_best_Defence = 8,
+        Remains_Of_The_Rust = 9,
+        Watten_against_Bots = 10,
+        The_Normal_Boxing_Game = 11,
+
+    }
+}

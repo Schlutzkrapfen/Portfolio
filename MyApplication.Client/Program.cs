4dc8b8415cd826dae25ba72c0b89400c18855bab
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-10-12 22:25:34 +0200

    Added the a mudcarusel that the more Pictures ares shown

--- MyApplication.Client/Program.cs
+++ MyApplication.Client/Program.cs
@@ -1,7 +1,13 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
+using System.Net.Http; 
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+// Add this line to register HttpClient
+builder.Services.AddScoped(sp => new HttpClient { 
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
+});
 
 builder.Services.AddMudServices();
 
--- MyApplication/Program.cs
+++ MyApplication/Program.cs
@@ -1,11 +1,25 @@
 using MudBlazor.Services;
 using MyApplication.Client.Pages;
 using MyApplication.Components;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using RouteAttribute = Microsoft.AspNetCore.Mvc.RouteAttribute; 
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add MudBlazor services
 builder.Services.AddMudServices();
+
+// Add HttpClient for Blazor components
+builder.Services.AddScoped(sp =>
+{
+    var navigationManager = sp.GetRequiredService<NavigationManager>();
+    return new HttpClient { BaseAddress = new Uri(navigationManager.BaseUri) };
+});
+
+// Add Controllers for the API
+builder.Services.AddControllers();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -28,13 +42,18 @@ else
 
 app.UseHttpsRedirection();
 
-
 app.UseAntiforgery();
 
 app.MapStaticAssets();
+
+// Map the API controllers
+app.MapControllers();
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(MyApplication.Client._Imports).Assembly);
 
 app.Run();
+
+// Move the controller to a separate f

6ea8b8448c177ae0ba92cd69e7dc03f603e92c8e
fhs49586 <fhs49586@fh-salzburg.ac.at>
2025-10-14 02:29:59 +0200

    changed some logik that it works on github pages and added a profile

--- MyApplication.Client/Program.cs
+++ MyApplication.Client/Program.cs
@@ -1,14 +1,27 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MudBlazor.Services;
 using System.Net.Http; 
+using MyApplication.Client;
 
+Console.WriteLine("Starting app...");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+Console.WriteLine("Builder created...");
+
+//Eigentlich sollte das #app sein
+builder.RootComponents.Add<App>("#app");
 // Add this line to register HttpClient
 builder.Services.AddScoped(sp => new HttpClient { 
     BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) 
 });
 
+Console.WriteLine("HttpClient registered...");
+
 builder.Services.AddMudServices();
 
+Console.WriteLine("MudServices added...");
+
 await builder.Build().RunAsync();
+
+Console.WriteLine("App running!");
+
